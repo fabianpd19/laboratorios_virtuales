9c2d27c3f67ac6a3b52ed2da3aff719b8126938f
fabianpd19 <fapalma1@espe.edu.ec>
2024-07-25 23:55:45 -0500

    Contenidos practicas 1 - 5 matemática

--- js/cuestionarios/matematica-preguntas.js
+++ js/cuestionarios/matematica-preguntas.js
@@ -158,8 +158,117 @@ const practica3_matematica = [
   },
 ];
 
+const practica4_matematica = [
+  {
+    question: "¿Cuál de las siguientes no es una característica de una recta?",
+    options: [
+      { type: "text", value: "Es un elemento unidimensional" },
+      { type: "text", value: "Está formada por una serie infinita de puntos" },
+      { type: "text", value: "Presenta curvas" },
+      { type: "text", value: "Va en una sola dirección" },
+    ],
+    correctOption: 3,
+  },
+  {
+    question:
+      "¿Qué elemento geométrico está formado por una serie finita de segmentos no colineales que forman un espacio cerrado?",
+    options: [
+      { type: "text", value: "Semirrecta" },
+      { type: "text", value: "Ángulo" },
+      { type: "text", value: "Segmento" },
+      { type: "text", value: "Polígono" },
+    ],
+    correctOption: 4,
+  },
+  {
+    question:
+      "¿Cuál de las siguientes características no distingue a los polígonos regulares de los irregulares?",
+    options: [
+      { type: "text", value: "Medida de sus lados" },
+      { type: "text", value: "Medida de sus ángulos interiores" },
+      { type: "text", value: "Simetría" },
+      { type: "text", value: "Cantidad de lados" },
+    ],
+    correctOption: 2,
+  },
+  {
+    question:
+      "La semirrecta es un elemento unidimensional que se prolonga al infinito en una sola dirección.",
+    options: [
+      { type: "text", value: "Verdadero" },
+      { type: "text", value: "Falso" },
+    ],
+    correctOption: 1,
+  },
+  {
+    question:
+      "Los polígonos regulares tienen todos sus lados y ángulos interiores iguales.",
+    options: [
+      { type: "text", value: "Verdadero" },
+      { type: "text", value: "Falso" },
+    ],
+    correctOption: 1,
+  },
+];
+
+const practica5_matematica = [
+  {
+    question:
+      "¿Cuál de los siguientes no es un elemento de la geometría plana?",
+    options: [
+      { type: "text", value: "Recta" },
+      { type: "text", value: "Semirrecta" },
+      { type: "text", value: "Plano" },
+      { type: "text", value: "Segmento" },
+    ],
+    correctOption: 3, // Respuesta: c. Plano
+  },
+  {
+    question:
+      "¿Qué figura geométrica plana tiene todos sus lados y ángulos iguales?",
+    options: [
+      { type: "text", value: "Triángulo equilátero" },
+      { type: "text", value: "Rombo" },
+      { type: "text", value: "Cuadrado" },
+      { type: "text", value: "Pentágono regular" },
+    ],
+    correctOption: 3, // Respuesta: c. Cuadrado
+  },
+  {
+    question:
+      "¿Qué herramienta de GeoGebra permite agregar el plano cartesiano al área de trabajo?",
+    options: [
+      { type: "text", value: "Mostrar ejes" },
+      { type: "text", value: "Crear puntos" },
+      { type: "text", value: "Dibujar polígonos" },
+      { type: "text", value: "Mostrar cuadrículas mayor y menor" },
+    ],
+    correctOption: 4, // Respuesta: d. Mostrar ejes y cuadrículas mayor y menor
+  },
+  {
+    question:
+      "Un segmento es un elemento unidimensional formado por una serie infinita de puntos que van en una sola dirección.",
+    options: [
+      { type: "text", value: "Verdadero" },
+      { type: "text", value: "Falso" },
+    ],
+    correctOption: 2, // Respuesta: Falso
+  },
+  {
+    question:
+      "GeoGebra es una herramienta matemática que permite trabajar únicamente con geometría en 2D.",
+    options: [
+      { type: "text", value: "Verdadero" },
+      { type: "text", value: "Falso" },
+    ],
+    correctOption: 2, // Respuesta: Falso
+  },
+];
+
 document.addEventListener("DOMContentLoaded", () => {
   startQuiz(1, practica1_matematica);
   startQuiz(2, practica2_matematica);
   startQuiz(3, practica3_matematica);
+  startQuiz(4, practica4_matematica);
+  startQuiz(5, practica5_matematica);
 });
