2f6e2aae143cd62a7173b7251108ee782b4145a1
fabianpd19 <fapalma1@espe.edu.ec>
2024-07-26 17:43:09 -0500

    Subir contenidos de los laboratorios de física

--- js/cuestionarios/fisica-preguntas.js
+++ js/cuestionarios/fisica-preguntas.js
@@ -52,6 +52,319 @@ const practica1_fisica = [
   },
 ];
 
+const practica2_fisica = [
+  {
+    question:
+      "¿Cuál es la función principal de una polea en un sistema mecánico?",
+    options: [
+      { type: "text", value: "a) Transmitir electricidad" },
+      { type: "text", value: "b) Modificar la dirección de una fuerza" },
+      { type: "text", value: "c) Generar calor" },
+      { type: "text", value: "d) Almacenar energía" },
+    ],
+    correctOption: 2,
+  },
+  {
+    question:
+      "¿Qué fórmula se utiliza para calcular la relación entre la fuerza aplicada y la resistencia en un sistema de poleas móviles?",
+    options: [
+      { type: "text", value: "a) F = R" },
+      { type: "text", value: "b) F = R / n" },
+      { type: "text", value: "c) T = F / n" },
+      { type: "text", value: "d) Fg = ∑Ti^2" },
+    ],
+    correctOption: 2,
+  },
+  {
+    question:
+      "Una polea simple se puede entender como una palanca de segundo grado.",
+    options: [
+      { type: "text", value: "a) Verdadero" },
+      { type: "text", value: "b) Falso" },
+    ],
+    correctOption: 2,
+  },
+  {
+    question:
+      "Las actividades prácticas en el laboratorio ayudan a aplicar conceptos teóricos y desarrollar habilidades de análisis.",
+    options: [
+      { type: "text", value: "a) Verdadero" },
+      { type: "text", value: "b) Falso" },
+    ],
+    correctOption: 1,
+  },
+  {
+    question:
+      "Es importante considerar las limitaciones y fuentes de error al realizar mediciones en un experimento de poleas.",
+    options: [
+      { type: "text", value: "a) Verdadero" },
+      { type: "text", value: "b) Falso" },
+    ],
+    correctOption: 1,
+  },
+];
+
+const practica3_fisica = [
+  {
+    question:
+      "¿Cuál de las siguientes afirmaciones sobre la fuerza de rozamiento por deslizamiento es correcta?",
+    options: [
+      {
+        type: "text",
+        value:
+          "a) La fuerza de rozamiento es independiente de la fuerza normal entre las superficies.",
+      },
+      {
+        type: "text",
+        value:
+          "b) La fuerza de rozamiento es proporcional al área de contacto entre las superficies.",
+      },
+      {
+        type: "text",
+        value:
+          "c) La fuerza de rozamiento es igual a la fuerza aplicada cuando el objeto está a punto de deslizar.",
+      },
+      {
+        type: "text",
+        value:
+          "d) La fuerza de rozamiento es constante durante el deslizamiento.",
+      },
+    ],
+    correctOption: 3,
+  },
+  {
+    question:
+      "¿Cuál es la fórmula para calcular la fuerza de rozamiento estático máxima?",
+    options: [
+      { type: "text", value: "a) Fr = μ · N" },
+      { type: "text", value: "b) Fs máx = μs · N" },
+      { type: "text", value: "c) Fs máx = μk · N" },
+      { type: "text", value: "d) Fr = μs · N" },
+    ],
+    correctOption: 2,
+  },
+  {
+    question:
+      "¿Cuál de los siguientes factores NO afecta el coeficiente de rozamiento entre dos superficies?",
+    options: [
+      { type: "text", value: "a) La rugosidad de las superficies" },
+      { type: "text", value: "b) La naturaleza de los materiales en contacto" },
+      { type: "text", value: "c) El área de contacto entre las superficies" },
+      { type: "text", value: "d) La fuerza normal aplicada" },
+    ],
+    correctOption: 3,
+  },
+  {
+    question:
+      "El coeficiente de rozamiento cinético es siempre mayor que el coeficiente de rozamiento estático.",
+    options: [
+      { type: "text", value: "a) Verdadero" },
+      { type: "text", value: "b) Falso" },
+    ],
+    correctOption: 2,
+  },
+  {
+    question:
+      "La fuerza de rozamiento entre dos cuerpos en contacto es independiente de la fuerza normal que actúa entre ellos.",
+    options: [
+      { type: "text", value: "a) Verdadero" },
+      { type: "text", value: "b) Falso" },
+    ],
+    correctOption: 2,
+  },
+];
+
+const practica4_fisica = [
+  {
+    question:
+      "¿Qué expresa la fórmula de la presión hidrostática P = ρgh + P0?",
+    options: [
+      {
+        type: "text",
+        value:
+          "a) La presión hidrostática depende de la densidad del líquido, la aceleración de la gravedad y la altura del líquido.",
+      },
+      {
+        type: "text",
+        value:
+          "b) La presión hidrostática depende solo de la altura del líquido.",
+      },
+      {
+        type: "text",
+        value:
+          "c) La presión hidrostática depende solo de la densidad del líquido.",
+      },
+      {
+        type: "text",
+        value:
+          "d) La presión hidrostática depende solo de la aceleración de la gravedad.",
+      },
+    ],
+    correctOption: 1,
+  },
+  {
+    question:
+      "¿Cuál es la diferencia principal entre los gases y los líquidos?",
+    options: [
+      {
+        type: "text",
+        value:
+          "a) Los gases son compresibles y los líquidos son incompresibles.",
+      },
+      {
+        type: "text",
+        value:
+          "b) Los gases ocupan todo el volumen del recipiente y los líquidos adoptan la forma del recipiente.",
+      },
+      {
+        type: "text",
+        value: "c) Los gases tienen mayor densidad que los líquidos.",
+      },
+      { type: "text", value: "d) Ambas a) y b) son correctas." },
+    ],
+    correctOption: 4,
+  },
+  {
+    question:
+      "La presión hidrostática es independiente de la dirección en la que se mida.",
+    options: [
+      { type: "text", value: "a) Verdadero" },
+      { type: "text", value: "b) Falso" },
+    ],
+    correctOption: 1,
+  },
+  {
+    question: "La presión hidrostática aumenta linealmente con la profundidad.",
+    options: [
+      { type: "text", value: "a) Verdadero" },
+      { type: "text", value: "b) Falso" },
+    ],
+    correctOption: 1,
+  },
+  {
+    question:
+      "La diferencia de presión entre dos puntos de un fluido depende solo de la diferencia de altura entre ellos.",
+    options: [
+      { type: "text", value: "a) Verdadero" },
+      { type: "text", value: "b) Falso" },
+    ],
+    correctOption: 1,
+  },
+];
+
+const practica5_fisica = [
+  {
+    question: "¿Cuál es la unidad de medida del trabajo?",
+    options: [
+      { type: "text", value: "a) Newton" },
+      { type: "text", value: "b) Vatio" },
+      { type: "text", value: "c) Joule" },
+      { type: "text", value: "d) Metro" },
+    ],
+    correctOption: 3,
+  },
+  {
+    question: "¿Qué representa la potencia?",
+    options: [
+      { type: "text", value: "a) La rapidez con la que se realiza el trabajo" },
+      { type: "text", value: "b) La fuerza aplicada" },
+      { type: "text", value: "c) La distancia recorrida" },
+      { type: "text", value: "d) La masa del objeto" },
+    ],
+    correctOption: 1,
+  },
+  {
+    question: "¿Cuál es la fórmula para calcular el trabajo?",
+    options: [
+      { type: "text", value: "a) W = F * d" },
+      { type: "text", value: "b) W = F * d * cos(θ)" },
+      { type: "text", value: "c) W = P * t" },
+      { type: "text", value: "d) W = m * g" },
+    ],
+    correctOption: 2,
+  },
+  {
+    question:
+      "El trabajo siempre se realiza cuando hay un desplazamiento en la dirección de la fuerza aplicada.",
+    options: [
+      { type: "text", value: "a) Verdadero" },
+      { type: "text", value: "b) Falso" },
+    ],
+    correctOption: 1,
+  },
+  {
+    question:
+      "La potencia es independiente del tiempo empleado para realizar el trabajo.",
+    options: [
+      { type: "text", value: "a) Verdadero" },
+      { type: "text", value: "b) Falso" },
+    ],
+    correctOption: 2,
+  },
+];
+
+const practica6_fisica = [
+  {
+    question:
+      "¿Cuál es la fórmula matemática que describe el coeficiente de resorte equivalente (Keq) para un sistema de dos resortes?",
+    options: [
+      { type: "text", value: "a) Keq = (K1 + K2) / 2" },
+      { type: "text", value: "b) Keq = K1 + K2" },
+      { type: "text", value: "c) Keq = 2(K1 + K2)" },
+      { type: "text", value: "d) Keq = K1 * K2" },
+    ],
+    correctOption: 1,
+  },
+  {
+    question:
+      "¿Qué unidad de medida se utiliza para la constante elástica del muelle (k) según el Sistema Internacional?",
+    options: [
+      { type: "text", value: "a) Newton por metro (N/m)" },
+      { type: "text", value: "b) Julio (J)" },
+      { type: "text", value: "c) Kilogramo (kg)" },
+      { type: "text", value: "d) Metro cuadrado (m^2)" },
+    ],
+    correctOption: 1,
+  },
+  {
+    question:
+      "¿Qué representa la energía potencial elástica en un cuerpo unido a un muelle?",
+    options: [
+      { type: "text", value: "a) La energía cinética del cuerpo" },
+      { type: "text", value: "b) La energía térmica generada por la fricción" },
+      {
+        type: "text",
+        value: "c) La energía almacenada debido a la deformación elástica",
+      },
+      { type: "text", value: "d) La energía gravitatoria del cuerpo" },
+    ],
+    correctOption: 3,
+  },
+  {
+    question:
+      "La ley de Hooke establece que la fuerza aplicada a un muelle es inversamente proporcional a la deformación que se le produce.",
+    options: [
+      { type: "text", value: "a) Verdadero" },
+      { type: "text", value: "b) Falso" },
+    ],
+    correctOption: 2,
+  },
+  {
+    question:
+      "La constante elástica del muelle (k) depende del material del muelle y su valor indica cuánto trabajo cuesta estirar el muelle.",
+    options: [
+      { type: "text", value: "a) Verdadero" },
+      { type: "text", value: "b) Falso" },
+    ],
+    correctOption: 1,
+  },
+];
+
 document.addEventListener("DOMContentLoaded", () => {
   startQuiz(1, practica1_fisica);
+  startQuiz(2, practica2_fisica);
+  startQuiz(3, practica3_fisica);
+  startQuiz(4, practica4_fisica);
+  startQuiz(5, practica5_fisica);
+  startQuiz(6, practica6_fisica);
 });
